cc156f31f3aa78d0d6b5fb75d25cb972293c82a1
morgansj <46675972+morgansj@users.noreply.github.com>
2023-09-14 15:30:35 +0100

    Initial commit

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   subnet_id     = "subnet-080501e54791222b4"
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
 
   tags = {
     Name = "HelloWorld"
