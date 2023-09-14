7eb22545abf137fa6852dff2f00d4881e8f50878
morgansj <46675972+morgansj@users.noreply.github.com>
2023-09-14 18:27:36 +0100

    use module for security group

--- main.tf
+++ main.tf
@@ -23,11 +23,25 @@ resource "aws_instance" "blog" {
   subnet_id     = "	subnet-085d41e0512e133f5"
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog_new"
+
+  vpc_id  = "vpc-0d4677025063571e0"
+
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
