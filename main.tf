93d1f3145492f4a03d06398ea7bbd17740d6bfe5
morgansj <46675972+morgansj@users.noreply.github.com>
2023-09-15 14:50:30 +0100

    changed subnet id

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "vpc" {
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
-  subnet_id     = "	subnet-085d41e0512e133f5"
+  subnet_id     = "subnet-0d1d2819389573b4a"
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
@@ -47,7 +47,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "blog_new"
 
-  vpc_id  = "module.vpc.public_subnets[0]"
+  #vpc_id  = "module.vpc.public_subnets[0]"
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
