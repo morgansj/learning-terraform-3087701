34988f2dc3bc501a05e29343bd34acd59d18b80b
morgansj <46675972+morgansj@users.noreply.github.com>
2023-09-15 14:55:22 +0100

    Added

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "blog_new"
 
-  vpc_id  = "module.vpc.public_subnets[0]"
+  vpc_id  = "vpc-0d8fff49f495e099a"
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
